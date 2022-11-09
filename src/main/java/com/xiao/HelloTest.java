79e998cada01578d20c2804c7dc412675d861ac6
xiao20001218 <117699795+xiao20001218@users.noreply.github.com>
2022-11-09 14:40:56 +0800

    Update HelloTest.java

--- src/main/java/com/xiao/HelloTest.java
+++ src/main/java/com/xiao/HelloTest.java
@@ -7,6 +7,7 @@ public class HelloTest {
         System.out.println("hello welcome to v1.0 Git");
         System.out.println("How are you");
         System.out.println("I'm fine");
+        sum(10,5);
     }
 
     public int sum(int a, int b) {
