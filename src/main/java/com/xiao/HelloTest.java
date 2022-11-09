0bc705c020e2d9c908d04e7e58d794aac0afa05b
xiao <2041671461@qq.com>
2022-11-09 09:17:33 +0800

    welcome to master Git

--- src/main/java/com/xiao/HelloTest.java
+++ src/main/java/com/xiao/HelloTest.java
@@ -4,6 +4,6 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello welcome to Git");
+        System.out.println("hello welcome to master Git");
     }
 }
