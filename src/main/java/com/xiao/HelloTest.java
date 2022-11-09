6e74fc3833a605a0ee909a86ab202777fcd5faf3
xiao <2041671461@qq.com>
2022-11-09 09:25:47 +0800

    line 3 How are you

--- src/main/java/com/xiao/HelloTest.java
+++ src/main/java/com/xiao/HelloTest.java
@@ -5,5 +5,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello welcome to v1.0 Git");
+        System.out.println("How are you");
     }
 }
